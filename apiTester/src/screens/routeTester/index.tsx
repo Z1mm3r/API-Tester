e4c53956fc275b73490ac8cf9772ead95f88c518
Z1mm3r <czimmerdev@gmail.com>
2023-11-06 19:36:47 -0500

    added ability to create values for reqeust body

--- apiTester/src/screens/routeTester/index.tsx
+++ apiTester/src/screens/routeTester/index.tsx
@@ -1,6 +1,11 @@
-import { useState, useCallback, useEffect } from "react";
+import { useState, useCallback, useEffect, useMemo } from "react";
 import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 import { Button, Card, TextField, Typography } from "@mui/material";
+
+interface bodyFields {
+    name: string;
+    value: string;
+}
 
 const RouteTester = () => {
 
@@ -11,20 +16,7 @@ const RouteTester = () => {
     const [urlText, setURLText] = useState("http://127.0.0.1:3001");
     const url = "http://127.0.0.1:3001";
 
-    //Testing
-    useEffect(() => {
-        fetch(`${urlText}/api/recipes`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        })
-            .then(res => res.text())
-            .then((data) => {
-                console.log(data)
-                setResponseText(data)
-            })
-    }, [])
+    const [bodyFields, setBodyFields] = useState<bodyFields[]>([]);
 
     const initiateRequest = useCallback(() => {
         const options = {
@@ -43,9 +35,51 @@ const RouteTester = () => {
             })
     }, [pathText, methodText, text])
 
+    const updateBodyField = (index: number, name: string, value: string) => {
+        setBodyFields(bodyFields.map((element, currentIndex) => {
+            if (currentIndex == index) {
+                return { name: name, value: value }
+            }
+            else {
+                return element;
+            }
+        }))
+    }
+
+    const addBodyField = useCallback(() => {
+        let newBodyField: bodyFields = { name: "", value: "" }
+        setBodyFields([...bodyFields, newBodyField]);
+    }, [bodyFields])
+
     useEffect(() => {
         console.log(text);
     }, [text])
+
+    const renderBodyField = (bodyField: { name: string, value: string }, index: number) => {
+        return (
+            <Grid2 xs={5} key={`bodyField-${index}`} >
+                <TextField value={bodyField.name} key={`bodyField-${index}-name`} label="Property Name"
+                    onChange={(e) => {
+                        updateBodyField(index, e.target.value, bodyFields[index].value);
+                    }}
+                />
+
+                <TextField value={bodyField.value} key={`bodyField-${index}-value `} label="Property Value"
+                    onChange={(e) => {
+                        updateBodyField(index, bodyFields[index].name, e.target.value);
+                    }}
+                />
+            </Grid2>
+        )
+    }
+    const renderBodyFields = useMemo(() => {
+
+        return (
+            <Grid2 container>
+                {bodyFields.map((bodyField, index) => renderBodyField(bodyField, index))}
+            </Grid2>
+        );
+    }, [bodyFields.length])
 
     return (
         <Grid2 container alignItems="center" justifyContent="center" direction="column">
@@ -63,6 +97,18 @@ const RouteTester = () => {
                 <Grid2 xs={8} padding={2}></Grid2>
 
             </Grid2 >
+            <Grid2 xs={12} display="flex" alignItems="center" justifyContent="center">
+                <Grid2 container alignItems="center" justifyContent="center" direction="column">
+                    <Grid2 xs={12} display="flex" alignItems="center" justifyContent="center">
+                        {renderBodyFields}
+                    </Grid2>
+                    <Grid2>
+                        <Button onClick={() => { addBodyField() }}>
+                            Add additional body field
+                        </Button>
+                    </Grid2>
+                </Grid2>
+            </Grid2>
             <Grid2 xs={8} display="flex" alignItems="center" justifyContent="center">
                 <Button variant="outlined" onClick={initiateRequest}>
                     Submit
